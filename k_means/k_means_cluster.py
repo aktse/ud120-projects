1491521c24c5db12ff7395de0f9899cce0f48757
Aaron Tse <akt@ualberta.ca>
2017-07-11 21:24:15 -0600

    Completed k-means mini project

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
     Skeleton code for k-means clustering mini-project.
 """
 
@@ -40,31 +40,49 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
+### Compute the min and max of of our featuresfor feature scaling
+def findMinMax(dict, feature_name):
+    min = 9999999999
+    max = 0
+    for values in dict.values():
+        v = values[feature_name]
+        if (v == 'NaN'):
+            continue
+        if (v > max):
+            max = v
+        if (v < min):
+            min = v
+    print "{}: Min: {}, Max: {}".format(feature_name, min, max)
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+findMinMax(data_dict, "exercised_stock_options")
+findMinMax(data_dict, "salary")
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+cluster = KMeans(n_clusters=2)
+pred = cluster.fit_predict(finance_features, poi)
 
 
 
